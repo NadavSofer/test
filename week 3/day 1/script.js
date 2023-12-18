bcc964a06405073e95811ad9908b0239047a8eb2
NadavSofer <nadavsof5@gmail.com>
2023-12-18 20:08:18 +0200

    week3 day1

---
+++ week 3/day 1/script.js
@@ -0,0 +1,18 @@
+let thing = 'hello world'
+
+// console.log(thing);
+
+let a = 5
+let b = true;
+let c = 5+20
+
+// console.log(b);
+// console.log(a + c);
+
+// console.log(a*c+(b+a-1));
+
+
+
+let string = `look it's the number ${a*5}`;
+let sub = string.substring(0,4)
+console.log(sub);
