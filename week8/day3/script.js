ea64ffa49f074da3c2d12124d46d21e265f63f2a
NadavSofer <nadavsof5@gmail.com>
2024-03-12 21:05:45 +0200

    week8 day3 part2

--- week8/day3/script.js
+++ week8/day3/script.js
@@ -15,7 +15,6 @@
 //     for (let index = 0; index < array.length; index++) {
 //         const element = array[index];
 //         if (condition) {
-            
 //         }
 //     }
 // }
@@ -26,26 +25,87 @@
 //     let a = 5
 // }
 
-function sayHello(name='guest') {
-    console.log('Hello', name);
-}
-sayHello('Sarah')
-sayHello('Shosh')
-sayHello()
-
-let age = 18
-
-// if (age < 18) {
-//     console.log("Sorry, you're too young.");
-// } else if (age > 18) {
-//     console.log("Yay! You're old enough!");
-// } else {
-//     console.log();
+// function sayHello(name='guest') {
+//     console.log('Hello', name);
 // }
+// sayHello('Sarah')
+// sayHello('Shosh')
+// sayHello()
 
-age < 18 ? console.log("Sorry, you're too young.") : 
-age > 18 ? console.log("Yay! You're old enough!") : 
-console.log('exactly');
+let age = 28
+
+if (age < 18) {
+    console.log("Sorry, you're too young.");
+} 
+else if (age > 18) {
+    console.log("Yay! You're old enough!");
+} 
+else {
+    console.log();
+}
+
+// age < 18 ? console.log("Sorry, you're too young.") : 
+// age > 18 ? console.log("Yay! You're old enough!") : 
+// console.log('exactly');
 
 
-// (condition) ? if true : if false
+// condition ? if true : if false
+
+age > 21 ? console.log('older') : 
+age < 21 ? console.log('younger') : 
+console.log(false);
+
+let person = ''
+
+if (person == '') {
+    console.log('מלך');
+} else {
+    console.log('איכס');
+}
+
+person == '' ? console.log('מלך') : console.log('איכס');
+
+let year = 2024
+
+let str = `hello class, 
+the year is ${year}`
+
+// ---------------------------------------
+
+funcName()
+
+function funcName () {
+
+}
+
+const funcName2 = () => {
+
+}
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+const add2 = (num1, num2) => num1 + num2
+
+const sayHey = name => {
+    console.log(`hey, ${name}`);
+}
+
+const times2 = num => num*2 
+
+function times(num) {
+    return num*2 
+}
+
+console.log(times2(25));
+
+const calculator = (num1, num2, sign) => 
+    sign == '+' ? num1 + num2 :
+    sign == '-' ? num1 - num2 :
+    sign == '*' ? num1 * num2 : 
+    console.log('not supported');
+
+console.log(calculator(2,5,'*'))
+console.log(calculator(2,5,'-'))
+console.log(calculator(2,5,'+'))
