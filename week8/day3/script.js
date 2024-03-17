da78f3f356c6c17d9c744a51eeb9f0d687ec6182
NadavSofer <nadavsof5@gmail.com>
2024-03-17 21:01:19 +0200

    week8 day3 part3

--- week8/day3/script.js
+++ week8/day3/script.js
@@ -32,17 +32,17 @@
 // sayHello('Shosh')
 // sayHello()
 
-let age = 28
+// let age = 28
 
-if (age < 18) {
-    console.log("Sorry, you're too young.");
-} 
-else if (age > 18) {
-    console.log("Yay! You're old enough!");
-} 
-else {
-    console.log();
-}
+// if (age < 18) {
+//     console.log("Sorry, you're too young.");
+// } 
+// else if (age > 18) {
+//     console.log("Yay! You're old enough!");
+// } 
+// else {
+//     console.log();
+// }
 
 // age < 18 ? console.log("Sorry, you're too young.") : 
 // age > 18 ? console.log("Yay! You're old enough!") : 
@@ -51,61 +51,102 @@ else {
 
 // condition ? if true : if false
 
-age > 21 ? console.log('older') : 
-age < 21 ? console.log('younger') : 
-console.log(false);
+// age > 21 ? console.log('older') : 
+// age < 21 ? console.log('younger') : 
+// console.log(false);
 
-let person = ''
+// let person = ''
 
-if (person == '') {
-    console.log('מלך');
-} else {
-    console.log('איכס');
-}
+// if (person == '') {
+//     console.log('מלך');
+// } else {
+//     console.log('איכס');
+// }
 
-person == '' ? console.log('מלך') : console.log('איכס');
+// person == '' ? console.log('מלך') : console.log('איכס');
 
-let year = 2024
+// let year = 2024
 
-let str = `hello class, 
-the year is ${year}`
+// let str = `hello class, 
+// the year is ${year}`
 
-// ---------------------------------------
+// // ---------------------------------------
 
-funcName()
+// funcName()
 
-function funcName () {
+// function funcName () {
 
-}
+// }
 
-const funcName2 = () => {
+// const funcName2 = () => {
 
-}
+// }
 
-const add = (num1, num2) => {
-    return num1 + num2
-}
+// const add = (num1, num2) => {
+//     return num1 + num2
+// }
 
-const add2 = (num1, num2) => num1 + num2
+// const add2 = (num1, num2) => num1 + num2
 
-const sayHey = name => {
-    console.log(`hey, ${name}`);
-}
+// const sayHey = name => {
+//     console.log(`hey, ${name}`);
+// }
 
-const times2 = num => num*2 
+// const times2 = num => num*2 
 
-function times(num) {
-    return num*2 
-}
+// function times(num) {
+//     return num*2 
+// }
 
-console.log(times2(25));
+// console.log(times2(25));
 
-const calculator = (num1, num2, sign) => 
-    sign == '+' ? num1 + num2 :
-    sign == '-' ? num1 - num2 :
-    sign == '*' ? num1 * num2 : 
-    console.log('not supported');
+// const calculator = (num1, num2, sign) => 
+//     sign == '+' ? num1 + num2 :
+//     sign == '-' ? num1 - num2 :
+//     sign == '*' ? num1 * num2 : 
+//     console.log('not supported');
 
-console.log(calculator(2,5,'*'))
-console.log(calculator(2,5,'-'))
-console.log(calculator(2,5,'+'))
+// console.log(calculator(2,5,'*'))
+// console.log(calculator(2,5,'-'))
+// console.log(calculator(2,5,'+'))
+
+//---------------------------------------------------
+
+let arr = [1,2,3,4,5]
+arr.forEach((item,index) => {
+    console.log(`
+    number is ${item}
+    index is ${index}`);
+    arr[index] = arr[index] * 2
+})
+console.log(arr);
+
+
+const numbers = [10,11,12,15,20];
+
+numbers.forEach((num, i) => {
+    num % 2 == 0 ? console.log(num) : undefined
+})
+
+let ifTrue = numbers.some((value) => {
+    return (value < 13)
+})
+
+let ifAllTrue = numbers.every((value) => {
+    return (value > 5)
+})
+
+
+const words = ["wow","hey","bye"];
+
+let ifStartH = words.some((value) => {
+    return (value[0] == 'h')
+})
+
+console.log(ifStartH);
+
+//------------------------------
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.includes('Banana', 1));
+
