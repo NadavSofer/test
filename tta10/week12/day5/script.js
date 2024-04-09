44d9b318ce5179e0a760a418d57490247a1f7a6e
NadavSofer <nadavsof5@gmail.com>
2024-04-09 18:44:13 +0300

    week12 day5

---
+++ tta10/week12/day5/script.js
@@ -0,0 +1,8 @@
+// C:\Users\nadav\OneDrive\שולחן העבודה\di\tta10\week5\day3\review\forms\index.html
+
+// ..\..\tta10\week5\day3\review\forms\index.html
+
+let {sayHello, myName} = require('./script2')
+
+sayHello()
+console.log(myName + ' 25');
---
+++ tta10/week12/day5/script2.js
@@ -0,0 +1,10 @@
+function sayHello() {
+    console.log('hello world!!!');
+}
+
+let myName = 'nadav'
+
+module.exports = {
+    sayHello,
+    myName,
+}
